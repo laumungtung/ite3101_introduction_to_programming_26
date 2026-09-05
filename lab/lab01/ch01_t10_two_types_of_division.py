7f4f3bbe1202b4ff95e73cc3546cc8901eb63921
laumungtung <260636409@stu.vtc.edu.hk>
2026-09-05 17:19:57 +0000

    [skip actions] lab/lab01/ch01_t10_two_types_of_division.py

--- lab/lab01/ch01_t10_two_types_of_division.py
+++ lab/lab01/ch01_t10_two_types_of_division.py
@@ -1,10 +1,8 @@
-
-# 1. Define the variables
 cucumbers = 100
 num_people = 6
 
-# 2. Divide for whole cucumbers and print
-whole_cucumbers_per_person = cucumbers // num_people
+
+whole_cucumbers_per_person = cucumbers / num_people
 print(whole_cucumbers_per_person)
 
 
