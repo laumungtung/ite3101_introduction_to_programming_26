08717e541ebd515c05bba82df0e07129114e7eb3
laumungtung <260636409@stu.vtc.edu.hk>
2026-09-09 17:15:36 +0000

    [skip actions] lab/lab01/ch01_t13_value_error.py

--- lab/lab01/ch01_t13_value_error.py
+++
@@ -1,8 +0,0 @@
-float_1 = 0.25
-float_2 = 40.0
-
-Skill_Completed = "Python Syntax"
-exercise_completed = 13
-points_per_exercise = 5
-Point_total = 100
-Point_total += exercise_completed * points_per_exercise
