53fc9ac061502861a0d5d9b3a4a13ea00d034b35
laumungtung <260636409@stu.vtc.edu.hk>
2026-09-13 15:26:36 +0000

    lab/lab02/ch02_t03_escaping_characters.py

--- lab/lab02/ch02_t03_escaping_characters.py
+++ lab/lab02/ch02_t03_escaping_characters.py
@@ -2,5 +2,4 @@
 # Uncomment the following line!
 # 'This isn't flying, this is falling with style!'
 
-'This isn\'t flying this is falling with style!'
-
+'This isn\'t flying, this is falling with style!'
