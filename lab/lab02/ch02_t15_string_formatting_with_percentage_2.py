296f9ca273a1b5a912a0e95497f295d2dc7276cb
laumungtung <260636409@stu.vtc.edu.hk>
2026-09-18 10:35:42 +0000

    [skip actions] lab/lab02/ch02_t15_string_formatting_with_percentage_2.py

--- lab/lab02/ch02_t15_string_formatting_with_percentage_2.py
+++ lab/lab02/ch02_t15_string_formatting_with_percentage_2.py
@@ -1,7 +1,7 @@
 name = "Angela"("What is your name? ")
 quest = "Why this world needs coffee code"("What is your quest?")
 color = "laser color"("What is your favorite color?")
-
+ 
 # Uncomment the below 2 line of code!
 # print("Ah, so your name is ___, your quest is ___, "
 # "and your favorite color is ___." ___ (name, quest, color))
