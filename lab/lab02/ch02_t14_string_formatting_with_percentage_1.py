636dc011e4de5598aa8f08132e196c741d61a791
laumungtung <260636409@stu.vtc.edu.hk>
2026-09-18 10:29:26 +0000

    [skip actions] lab/lab02/ch02_t14_string_formatting_with_percentage_1.py

--- lab/lab02/ch02_t14_string_formatting_with_percentage_1.py
+++ lab/lab02/ch02_t14_string_formatting_with_percentage_1.py
@@ -1,5 +1,5 @@
 string_1 = "Camelot"
 string_2 = "place"
 
-print("Let's not go to"%(string_1)) 
-print("Tis a silly"%(string_2))
+print("Let's not go to" % (string_1))
+print("Tis a silly" % (string_2))
