a70f3e68affaadd7a866472d9703059a430a0f37
laumungtung <260636409@stu.vtc.edu.hk>
2026-09-18 11:26:09 +0000

    [skip actions] lab/lab02/ch02_t16_sth_completely_familiar.py

--- lab/lab02/ch02_t16_sth_completely_familiar.py
+++ lab/lab02/ch02_t16_sth_completely_familiar.py
@@ -1,10 +1,8 @@
 # Write your code below, starting on line 3!
 my_string = "the code is awesome"
-second_string = "alpha" 
+second_string = "alpha"
 
 len("my_string")
 "second_string".upper()
 
 print("my_string")
-
-
